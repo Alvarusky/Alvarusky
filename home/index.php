9776a4dff9080af56ed6286c84c5775ba1386994
Alvarusky <ademiguel.gd2@gmail.com>
2019-04-12 15:25:14 +0100

    whatever

--- home/index.php
+++ home/index.php
@@ -9,106 +9,7 @@ if(isset($_SESSION['uname'])){ //if login in session is not set
 <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Raleway:200" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Laila:600" rel="stylesheet">
-<style>
-
-header {
-  background-color:  #36393e;
-  margin: 0 auto;
-  padding: 0 auto;
-}
-
-body, html {
-  margin: 0;
-  padding: 0;
-  background-color:  #36393e;
-}
-
-#navbar {
-  width: 100%;
-  overflow: hidden;
-  background-color: #2b2d31; //#4a4c51;
-//Código para ponerle sombra a la navigation bar.
-  -webkit-box-shadow: 0 8px 6px -6px #999;
-  -moz-box-shadow: 0 8px 6px -6px #999;
-   box-shadow: 0 8px 6px -6px #999;
-}
-
-#navbar a {
-  float: left;
-  margin 0;
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 20px;
-  font-size: 150%;
-  font-family: 'Indie Flower';
-  text-decoration: none;
-}
-
-/* Change the link color to #111 (black) on hover */
-#navbar a:hover {
-  background-color: #4a4c51;
-}
-
-#navbar .active {
-  background-color:  #6666ff;
-}
-
-.sticky {
-  position: fixed;
-  top: 0;
-  width: 100%;
-}
-
-.sticky + .content {
-  padding-top: 60px;
-}
-
-header h1 {
- margin: 15px;
- padding-left:5%;
- text-align:center;
- font-family: 'Indie Flower';
- font-size: 700%;
- color: #ffffff; // #a0a4ac;
-}
-
-img {
-  position: absolute;
-  float:left;
-  width: 180px;
-  top: 0px;
-  height: 180px;
-  padding-left: 100px;
-}
-#pass {
-  margin-left: 13%;
-  margin-top: 20px;
-  font-family: 'Raleway';
-  font-size: 160%;
-  color: white;
-}
-#pass pre{
-  display: inline;
-  padding-left: 15px;
-  font-family: 'Raleway';
-}
-
-#pass p{
-  display: inline-block;
-  font-size: 110%;
-  padding-top: 0px;
-}
-
-.mpass{
-  margin-top: 45  px;
-  margin-left: 12%;
-  color: #1bbe1b;
-  font-family: 'Laila';
-  font-size: 400%;
-}
-
-</style>
+<style type = 'text/css' src = 'home.css'></style>
 <header>
 <a href="index.php" style = "text-decoration: none">
   <img src = '/images/logo.png'>
@@ -156,30 +57,6 @@ function stickyNavBar() {
     navbar.classList.remove('sticky');
   }
 }
-
-mirabalPass();
-
-function mirabalPass() {
-
-  var date = new Date();
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  var v2, v3, v4, v5, v7, v8;
-
-  calc = (200*day)+(3*month);
-  v2 = 20000 + calc;
-  v3 = 30000 + calc;
-  v4 = 40000 + calc;
-  v5 = 50000 + calc;
-  v7 = 70000 + calc;
-  v8 = 80000 + calc;
-
-  document.getElementById('v2').innerHTML = v2;
-  document.getElementById('v3').innerHTML = v3;
-  document.getElementById('v4').innerHTML = v4;
-  document.getElementById('v5').innerHTML = v5;
-  document.getElementById('v7').innerHTML = v7;
-  document.getElementById('v8').innerHTML = v8;
-}
 </script>
+<script type = 'text/javascript' src = 'mirabalPass.js'></script>
 </html>
