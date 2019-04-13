40fc50aa0a147de09fbfb868a9e6e96a771ec8fe
Alvarusky <ademiguel.gd2@gmail.com>
2019-04-13 17:05:55 +0100

    login version

--- login.php
+++ login.php
@@ -1,24 +1,138 @@
 <html>
+<style>
+
+header {
+  background-color:  #36393e;
+  margin: 0 auto;
+  padding: 0 auto;
+}
+
+body, html {
+  margin: 0;
+  padding: 0;
+  background-color:  #36393e;
+}
+
+#navbar {
+  width: 100%;
+  overflow: hidden;
+  background-color: #2b2d31; //#4a4c51;
+//Código para ponerle sombra a la navigation bar.
+  -webkit-box-shadow: 0 8px 6px -6px #999;
+  -moz-box-shadow: 0 8px 6px -6px #999;
+   box-shadow: 0 8px 6px -6px #999;
+}
+
+#navbar a {
+  float: left;
+  margin 0;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 20px;
+  font-size: 150%;
+  font-family: 'Indie Flower';
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+#navbar a:hover {
+  background-color: #4a4c51;
+}
+
+#navbar .active {
+  background-color:  #6666ff;
+}
+
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.sticky + .content {
+  padding-top: 60px;
+}
+
+h1 {
+ margin: 15px;
+ padding-left:5%;
+ text-align: center;
+ font-family: 'Indie Flower';
+ font-size: 700%;
+ color: #ffffff; // #a0a4ac;
+}
+
+img {
+  position: absolute;
+  float:left;
+  width: 180px;
+  top: 0px;
+  height: 180px;
+  padding-left: 100px;
+}
+
+#login {
+  text-align: center;
+  font-family: 'Raleway';
+}
+
+#txt_uname {
+  color: white;
+  height: 7%;
+  width: 20%;
+  border: 0;
+  outline: 0;
+  background: transparent;
+  border-bottom: 2px solid white;
+}
+
+#but_submit {
+  height: 6%;
+  width: 20%;
+  background: transparent;
+  color: white;
+}
+</style>
+<title>DemiChanclas</title>
+<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway:200" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Laila:600" rel="stylesheet">
+<link rel = 'stylesheet' href = '/home/home.css'></style>
 <header>
-<body>
+<a href="index.php" style = "text-decoration: none">
+  <img src = '/images/logo.png'>
+  <h1>Chancletas Chillonas</h1>
+</a>
+<div id= 'navbar'>
+<a class = 'active' href = 'index.php'>Home</a>
+<a href = 'data'>Datalogger</a>
+<a href = '/default/default.php'>Default Page</a>
+<a href = 'login.php'>Index.html</a>
+<a style = 'float:right' href = '/about'>About</a>
+</div>
+
+</header>
 <div class="container">
     <form method="post" action="">
-        <div id="div_login">
-            <h1>Login</h1>
-            <div>
+        <div>
+            <h1 style = 'text-align: center; font-family: "Laila"; font-size: 650%; width: 87%'>Login</h1>
+            <div id = 'login'>
+                <br>
+                <br>
                 <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
-            </div>
-            <div>
+                <br>
                 <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Password"/>
-            </div>
-            <div>
+                <br>
+                <br>
+                <br>
                 <input type="submit" value="Submit" name="but_submit" id="but_submit" />
+                <br>
             </div>
         </div>
     </form>
 </div>
 </body>
-</header>
 </html>
 <?php
 include "config.php";
