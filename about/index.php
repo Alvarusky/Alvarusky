123688522ce82e9ce352c4ab3ca33dbd0e4c1f12
Alvarusky <ademiguel.gd2@gmail.com>
2021-08-08 21:12:57 +0100

    New desing for about and home page

--- about/index.php
+++ about/index.php
@@ -1,102 +1,56 @@
-<?php
-
-include "localhost/config.php";
-session_start();
-// Check user login or not
-if(!isset($_SESSION['uname'])){
-    header('Location: /login.php');
-}
- ?>
 <html>
+  <head>
+    <title>DemiChanclas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Add jQuery, bootstrap and custom fonts -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-<meta name="viewport" content="width=device-width, initial-scale= 1">
-<link href="/fonts.css" rel="stylesheet">
+    <!-- Add CSS and fonts -->
+    <link rel= 'stylesheet' href= '/fonts.css'>
+    <link rel = 'stylesheet' href = '/home.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  </head>
+  <body>
 
-<meta name="viewport" content="width=device-width, initial-scale= 1">
-<title>About</title>
-
-<header>
-
-<!-- Navigation Bar -->
-<nav class= 'navbar navbar-expand-md fixed-top navbar-dark bg-dark'>
-  <a class= 'navbar-brand' href='/home/'>
+    <!-- Navigation bar -->
+    <nav class= 'main-nav navbar navbar-expand-md navbar-dark ps-2 py-0' style='background-color: #008080;'>
+      <a class= 'navbar-brand' href='#'>
         <img src= '/images/logo.jpeg' width= '30' height= '30' alt='' loading='lazy'>
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class= 'collapse navbar-collapse' id= 'navbarTogglerDemo02'>
-    <div class='navbar-nav mr-auto openSans'>
-      <a class = 'nav-item nav-link mx-3' href = '/home/'>Home</a>
-      <a class= 'nav-item nav-link mx-2' href = '#'>DHT11-T&H</a>
-      <a class= 'nav-item nav-link mx-2' href = '/cryptoChart'>CryptoCharts</a>
-      <a class= 'nav-item nav-link mx-2' href = '/how-to'>How To's</a>
-    </div>
-    <div class= 'navbar-nav ml-auto'> 
-      <a class= 'nav-item nav-link mx-2 active openSans'  href = '/about/'>About</a>
-      <span class= 'navbar-text mx-2'>|</span>
-      <a class= 'nav-item nav-link mx-2' href= '#' data-toggle='modal' data-target= '#logoutModal'>Log Out</a>
-    </div>
-  </div>
-</nav>
-
-</header>
-<body class= 'pt-5'>
-
-<!-- Log Out modal -->
-<div class= 'modal fade' id= 'logoutModal' tabindex= '-1' role= 'dialog' aria-labelledby= 'logoutModalLabel' aria-hidden= 'true'>
-  <div class= 'modal-dialog'>
-    <div class= 'modal-content bg-dark'>
-      <div class= 'modal-header'>
-        <h4 class= 'modal-title text-white openSans' id= 'logoutModalLabel'>Sign Up</h4>
-        <button type= 'button' class= 'close' data-dismiss= 'modal' aria-label= 'Close'> 
-          <span aria-hidden= 'true' style= 'color: white'>&times;</span>
-        </button>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class= 'collapse navbar-collapse' id= 'navbarTogglerDemo02'>
+        <div class='navbar-nav'>
+          <a class= 'nav-item nav-link mx-2' href = '/'>Home</a>
+          <a class= 'nav-item nav-link mx-2' href = '#'>DHT11-T&H</a>
+          <a class= 'nav-item nav-link mx-2' href = '/cryptoChart'>CryptoCharts</a>
+          <a class= 'nav-item nav-link mx-2' href = '/how-to/'>How To's</a>
+        </div>
+        <div class= 'navbar-nav ms-auto'>
+          <a class= 'nav-item nav-link mx-2'  href = '/about'>About</a>
+          <span class= 'navbar-text mx-2'>|</span>
+          <a class= 'nav-item nav-link mx-2' href= '#' data-toggle='modal' data-target= '#logoutModal'>Log In</a>
+        </div>
       </div>
-      <div class= 'modal-body'>
-	<p class= 'text-white raleway'> Are you sure you wanna log out? You won't be able to see any content published on this page.</p>
-	<p class= 'text-white raleway'> Press "Log Out" below to end your current session</p>
-      </div>
-      <div class= 'modal-footer openSans'>
-        <button type= 'button' class= 'btn btn-outline-secondary' data-dismiss= 'modal'>Cancel</button>
-        <button type= 'button' class= 'btn btn-light' onclick= 'window.location="/logout.php"'>Log Out</button>
-      </div>
+    </nav>
+
+    <!-- Main content -->
+    <div class='container-fluid mt-5'>
+      <h1 class='display-4 text-center fw-bold mb-5'>About this site</h1>
     </div>
-  </div>
-</div>
+
+      <div class= 'container-fluid pt-5 padded-content'>
+        <p>This website was first created by Alvaro de Miguel (<a href= 'https://twitter.com/_alvarodemiguel' target= 'blank'>@_alvarodemiguel</a>) on March 2019.
+          It's a LAMP server, hosted on a RaspberryPi Zero W, and you can find the source code on
+          <a href = 'https://github.com/Alvarusky/html' target = 'blank'>Github.</a>
+          I built this website cause I wanted to learn how Internet works and host my programs such as "CrytoCharts" or "DHT11-T&H" on the web.
+        </p>
+        <p> I love coding, so I decided to build my personal site.
+          I have other proyects as well, for instance, <a href="https://worldbook.ddns.net/" target= "blank">WorldBook</a>
+          which is an iniciative aimed at making books and knowledge available everywhere in the world.
+          At this time (10/6/2020) I'm only 16, so I've got no higher education, however I'm planning to take a double major in physics and math in Madrid.
+        </p>
+      </div>
 
 
-  <div class='container-fluid mt-5'>
-  <a href='/home/' class='ml-3'><img src='/images/home-blue.svg' onmouseover="hover(this);" onmouseout="unhover(this);" /></a>
-  <h1 class='display-2 text-center openSans'>About</h1>
-  <hr class= 'my-4 ml-5 mr-5'> 
-  <div class= 'container'>
-    <p class= 'text-left raleway m-5'>This website was first created by Alvaro de Miguel (<a href= 'https://twitter.com/_alvarodemiguel' target= 'blank'>@_alvarodemiguel</a>) on March 2019.
-    It's a LAMP server, hosted on a RaspberryPi Zero W, and you can find the source code on 
-    <a href = 'https://github.com/Alvarusky/html' target = 'blank'>Github.</a>
-    I built this website cause I wanted to learn how Internet works and host my programs such as "CrytoCharts" or "DHT11-T&H" on the web.
-    </p>
-    <p class= 'text-left raleway m-5'> I am a spanish teen who loves coding, and so I decided to build my personal site.
-      I'm also kind of entrepreneur, and <a href="https://worldbook.ddns.net/" target= "blank">WorldBook</a>
-      is my first startup.
-      At this time (10/6/2020) I'm only 16, so I've got no higher education, however I'm planning to take a double major in physics and math in Madrid.
-    </p>
-    </div>
-  </div>
-
-  <script>
-      function hover(element) {
-        element.setAttribute('src', '/images/home.svg');
-      }
-      function unhover(element) {
-        element.setAttribute('src', '/images/home-blue.svg');
-      }
-
-
-  </script>
-</body>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,83 @@
-<?php
-//redirect to login
-header("Location: login.php");
-?>
+<html>
+  <head>
+    <title>DemiChanclas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Add CSS and fonts -->
+    <link rel= 'stylesheet' href= '/fonts.css'>
+    <link rel = 'stylesheet' href = '/home.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <!-- Navigation bar -->
+    <nav class= 'main-nav navbar navbar-expand-md navbar-dark ps-2 py-0' style='background-color: #008080 ;'>
+      <a class= 'navbar-brand' href='#'>
+        <img src= '/images/logo.jpeg' width= '30' height= '30' alt='' loading='lazy'>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class= 'collapse navbar-collapse' id= 'navbarTogglerDemo02'>
+        <div class='navbar-nav'>
+          <a class= 'nav-item nav-link mx-2' href = '/home/'>Home</a>
+          <a class= 'nav-item nav-link mx-2' href = '#'>DHT11-T&H</a>
+          <a class= 'nav-item nav-link mx-2' href = '/cryptoChart'>CryptoCharts</a>
+          <a class= 'nav-item nav-link mx-2' href = '/how-to/'>How To's</a>
+        </div>
+        <div class= 'navbar-nav ms-auto'>
+          <a class= 'nav-item nav-link mx-2'  href = '/about'>About</a>
+          <span class= 'navbar-text mx-2'>|</span>
+          <a class= 'nav-item nav-link mx-2' href= '#' data-toggle='modal' data-target= '#logoutModal'>Log In</a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Main content -->
+    <div class='container-fluid mt-5'>
+      <h1 class='display-4 fw-bold text-center'>
+        Welcome to Alvaro de Miguel's personal site.
+      </h1>
+    </div>
+
+    <div class='container-fluid py-5 padded-content'>
+      <p class='h2'>- Presentation:</p>
+      <br>
+      <p>Welcome to my page guys, my name is alvaro, and this is the site where i host all my fun proyects, and maybe some thoughts i might have. Books, music, rocket science - all examples of stuff that i'm interested in.
+        The list goes on and on, and it even grows a bit from time to time. So don't be surprised to find new sections once in a while.
+        <br>
+        I was born in spain in 2003, and i am currently studying physics at the University of Seville (US).
+      </p>
+
+      <br>
+      <br>
+
+      <p class='h2'>- Purpose:</p>
+      <br>
+      <p> The purpose of this webpage isn't to be pretty (i mean just look at it) or to get thousands of views. It's more of a me to myself thing. Needless to say that other people are more than welcome to read my posts
+        and follow the progress i make on my proyects. Also, don't hesitate to contact me if you see any errors, or if you think you can contribute to a proyect.
+      </p>
+
+      <br>
+      <br>
+
+      <p class='h2'>- Contact:</p>
+      <br>
+      <p> You can contact me through social media (which i check out almost every day) or email:
+        <br>
+        instagram: <a href='https://www.instagram.com/alvarodemiguel3/'>@alvarodemiguel0</a>
+        <br>
+        twitter: <a href='https://twitter.com/_alvarodemiguel'>@_alvarodemiguel</a>
+        <br>
+        email: ademiguel.gd@gmail.com
+      </p>
+
+
+      <div class='d-flex justify-content-around mt-5'>
+        <a href='/hobbies'>go to hobbies</a>
+        <a href='/how-to'>go to how to's</a>
+      </div>
+    </div>
+
+  </body>
+</html>
