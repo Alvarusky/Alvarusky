540a5b10ed3c470c48e12afc1a97b3322543dd39
Alvarusky <ademiguel.gd2@gmail.com>
2022-01-25 21:55:46 +0100

    add photography section and fix collapse navbar

---
+++ hobbies/photography/index.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+
+  <?php include('../../nav.html'); ?>
+    <title>alvaro de miguel | hobbies</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Add CSS and fonts -->
+    <link rel= 'stylesheet' href= '/fonts.css'>
+    <link rel = 'stylesheet' href = '/home.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <!-- Directory location -->
+    <div class='container mt-4'><a href='/'><< </a>/<a href='/hobbies'>hobbies</a>/<a href='./'>photography</a>/</div>
+
+    <div class='container-fluid mt-5'>
+      <h1 class='display-4 text-center fw-bold mb-5'>photography</h1>
+    </div>
+
+    <div class= 'container py-5 padded-content'>
+        <p> These are some pictures that i've made. For the full collection and my latest photos you can visit my instagram's account: <a href='https://www.instagram.com/ad.mfotos/'> @ad.mfotos </a>
+    </div>
+
+    <div class= 'container'>
+      <div class= 'row justify-content-evenly mt-5'>
+        <div class= 'col-4'>
+          <img class= 'img-fluid' src= '/images/admfotos/boy_reading.JPG' />
+          <p class= 'text-center'> "Boy, reading". Artwork by ines de miguel. </p>
+        </div>
+
+        <div class='col-4'>
+          <img class= 'img-fluid' src= '/images/admfotos/Connection.JPG' />
+          <p class= 'text-center'> 
+            Conexión, def:
+              <br />
+              1. f. Acción y efecto de conectar.
+              2. f. Punto donde se realiza el enlace entre aparatos o sistemas.
+          </p>
+      </div>
+
+      <div class= 'row justify-content-evenly mt-5'>
+        <div class= 'col-4'>
+          <img class= 'img-fluid' src= '/images/admfotos/trip-to-oxford.jpg' />
+          <p class= 'text-center'>
+            trip to oxford, summer 2021.
+          </p>
+        </div>
+
+        <div class='col-4'>
+          <img class='img-fluid' src= '/images/admfotos/parques.jpg' />
+          <p class='text-center'>
+            parques...
+          </p>
+        </div>
+
+        <div class= 'row justify-content-evenly my-5'>
+        <div class= 'col-4'>
+          <img class= 'img-fluid' src= '/images/admfotos/backpack_traveller.JPG' />
+          <p class= 'text-center'>
+            backpack traveller talking to a homeless woman.
+          </p>
+        </div>
+
+        <div class='col-4'>
+          <img class='img-fluid' src= '/images/admfotos/dingle_beach.jpg' />
+          <p class='text-center'>
+          Nature is not a place to visit, it's our home. ~Gary Snyder
+          </p>
+        </div>
+
+        <div class= 'row justify-content-evenly mt-5'>
+        <div class= 'col-4'>
+          <img class= 'img-fluid' src= '/images/admfotos/mountain.png' />
+          <p class= 'text-center'>
+          Para ser tan vieja, albergas tanta vida…
+          Adiós, feliz montaña.
+            [Pirineos]
+          </p>
+        </div>
+
+        <div class='col-4'>
+          <img class='img-fluid' src= '/images/admfotos/simon_verde.jpg' />
+          <p class='text-center'>
+          a myriad of green tones in a simon verde garden.
+          </p>
+        </div>
+    </div>
+
+    <div class='container mt-4'><a href='/'><< </a>/<a href='/hobbies'>hobbies</a>/<a href='./'>photography</a>/</div>
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-  include('nav.html');
-?>
+<?php include('nav.html'); ?>
 <!DOCTYPE html>
 <html>
   <head>
