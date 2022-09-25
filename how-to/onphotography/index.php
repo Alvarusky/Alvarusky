fc96ab4a359d90b242062cd6b0e3fda019d32809
Alvarusky <ademiguel.gd2@gmail.com>
2022-09-25 13:58:56 +0200

    Make 'on photography' entry

---
+++ how-to/onphotography/index.php
@@ -0,0 +1,78 @@
+<?php
+  include('../../nav.html');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>alvaro de miguel | on photography</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Add CSS and fonts -->
+    <link rel= 'stylesheet' href= '/fonts.css'>
+    <link rel = 'stylesheet' href = '/home.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <!-- Main content -->
+    <div class='container mt-4'>
+      <a href='../../'><< </a>/<a href='../'>how to's</a>/<a href='./'>on photography</a>
+    </div>
+    <div class='container-fluid mt-5'>
+      <h1 class='display-4 text-center fw-bold'>On photography</h1>
+      <p class='text-center mb-5'> 02/02/2022 - 19/07/2022</p>
+    </div>
+
+    <div class= 'container-fluid'>
+      <div class= 'row justify-content-center pt-5'>
+        <div class= 'col-8'>
+
+          <p class='fw-bold'> I </p>
+          <p> Dont buy a new camera if you dont have one yet, cause cameras only capture what you already had pictured
+              -in your mind- as a photograph.  Instead, go out and try to make "mind pictures", as i like to call them.
+              "Mind pictures" are much easier to "take" cause you actually dont need to do anything but imagine you took a photo.
+              This way you will learn about composures and light pretty fast. 
+
+              <br>
+              <br>
+
+              Look for places where you can take good photographs. Such as parks, lakes or urban, and learn to portray them.
+              Get specialized in the things you like to photograph, though never stop innovating, giving them new perspectives.
+
+              <br>
+              <br>
+
+              PD: when you actually get a camera it's a good practice to keep taking mind pictures.
+          </p>
+          <p class='fw-bold'> II </p>
+          <p> Note: Although you should specialize in what you like to photograph, don't over-specialize nor specialize in 
+            very specific stuff. 
+            For instance, don't get specialized in clouds, but in skies. Play with them. Shoot a lot, and 
+            try to get cool perspectives and lightnings.
+
+            <br>
+
+            If you specialize too much in something, ur pictures will look all too much alike, making them 
+            soon repetitive and thus dull.
+
+            <br>
+
+            A good example to follow would be <a href='https://crisarbizzu.com/'>Cris Arbizzu</a>
+
+            </p>
+            <p class='fw-bold'> III </p>
+            <p> Always try to shoot photography that transmits a message to the viewer or makes an impression on him.
+            
+            <br>
+
+            Mine: Give them perspective.
+            </p>
+        </div>
+      </div>
+
+      <div class='container mt-5 mb-4'><a href='../../'><< </a>/<a href='../'>how to's</a>/<a href='./'>on photography</a></div>
+    </div>
+
+
+  </body>
+</html>
